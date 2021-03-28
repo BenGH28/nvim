f74d4318c9e95c6077b4bc6f1bdcd61a740d8543
Ben Hunt <bigben.hunt@gmail.com>
2021-03-27 21:03:25 -0600

    add some extra functionality to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-#helper functions
+### helper functions
 has_package(){
     if ! command -v $1 &>/dev/null; then
         false;
@@ -9,7 +9,35 @@ has_package(){
     fi
 }
 
-# LSP install functions
+MISSING_DEPENDS=false
+
+check_depends(){
+    if ! has_package $1;then
+        echo "missing $1";
+        MISSING_DEPENDS=true;
+    fi
+}
+has_dependencies(){
+    echo "checking dependencies..."
+    check_depends npm;
+    check_depends node;
+    check_depends rustup;
+    check_depends pip;
+    check_depends lua;
+    check_depends luarocks;
+
+    if [ $MISSING_DEPENDS == true ]; then
+        false;
+    else
+        true;
+    fi
+}
+
+clone_my_nvim_repo(){
+	git clone https://github.com/BenGH28/nvim.git $HOME/.config/nvim/
+}
+
+### LSP install functions
 install_bashls(){ npm i -g bash-language-server; }
 install_yamlls(){ npm install -g yaml-language-server; }
 install_vimls(){ npm install -g vim-language-server; }
@@ -28,39 +56,34 @@ install_efmls(){
 install_lua_format(){
     luarocks install --server=https://luarocks.org/dev luaformatter
 }
-#install my config of nvim into .config/nvim
-clone_my_nvim_repo(){
-	git clone https://github.com/BenGH28/nvim.git $HOME/.config/nvim/
-}
-
 install_npm_lsps(){
     install_bashls
     install_yamlls
     install_jsonls
-    install_vimlss
+    install_vimls
+}
+install_none_npm_lsps(){
+    install_pyls;
+    install_rls;
+    install_efmls;
+    install_lua_format;
 }
 
 
 install_all_lsps(){
-    #install all the npm lsps
-    has_npm=true
-    has_package npm && install_npm_lsps || \
-         echo "Npm missing. Cannot install bash, yaml, json, and vim lsps" && $has_npm=false
-
-    #install rls
-    has_rustup=true
-    has_package rustup && install_rls || echo "Rustup missing. Cannot install rls" && $has_rustup=false
-
-    #install pyls
-    has_pip=true
-    has_package pip && install_pip || echo "Pip missing. Cannot install pyls" && $has_pip=false
-
-    #install efmls
-    has_go=true
-    has_package go && install_efmls || echo "Golang missing. Cannot install efmls" && $has_pip=false
-
-    #install install_lua_format
-    has_luarocks=true
-    has_package lua && has_package luarocks && install_lua_format || \
-        echo "Lua and/or Luarocks missing. Cannot install luaformatter" && $has_luarocks=false
+    if has_dependencies ;
+    then
+        echo "dependencies passed. continue install?"
+        read
+        install_npm_lsps;
+        install_none_npm_lsps;
+    else
+        echo "you need to install some things first"
+    fi
 }
+
+
+###call the functions
+
+# clone_my_nvim_repo
+install_all_lsps
