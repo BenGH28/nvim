091ebc79bf0905da6eeea5e86b9b1a26bc69f7a8
Ben Hunt <bigben.hunt@gmail.com>
2021-10-07 12:20:28 -0600

    update nvim-tree config

--- lua/tree-conf.lua
+++ lua/tree-conf.lua
@@ -1,46 +1,65 @@
-vim.g.nvim_tree_side = 'left' -- left by default
-vim.g.nvim_tree_width = 40 -- 30 by default
-vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'} -- empty by default
-vim.g.nvim_tree_auto_open = 1 -- 0 by default, opens the tree when typing `vim $DIR` or `vim`
-vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
-vim.g.nvim_tree_auto_ignore_ft = {'startify', 'dashboard'} -- empty by default, don't auto open tree on specific filetypes.
-vim.g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
+vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache", ".undo"} -- empty by default
 vim.g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
 vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-vim.g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
-vim.g.nvim_tree_tab_open = 1 -- 0 by default, will open the tree when entering a new tab and the tree was previously open
-vim.g.nvim_tree_width_allow_resize = 1 -- 0 by default, will not resize the tree when opening a file
-vim.g.nvim_tree_disable_netrw = 1 -- 1 by default, disables netrw
-vim.g.nvim_tree_hijack_netrw = 1 -- 1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
-vim.g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-vim.g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1}
+vim.g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1, folder_arrows = 1}
 
 vim.g.nvim_tree_icons = {
-    default = '',
-    symlink = '',
-    git = {
-        unstaged = "✗",
-        staged = "✓",
-        unmerged = "",
-        renamed = "➜",
-        untracked = "★"
-    },
-    folder = {
-        default = "",
-        open = "",
-        empty = "",
-        empty_open = "",
-        symlink = ""
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "✗",
+    staged = "✓",
+    unmerged = "",
+    renamed = "➜",
+    untracked = "★"
+  },
+  folder = {
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = ""
+  }
+}
+
+require "nvim-tree".setup {
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = true,
+  ignore_ft_on_setup = {"startify"},
+  update_to_buf_dir = {
+    enable = true,
+    auto_open = true
+  },
+  auto_close = true,
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = false,
+  lsp_diagnostics = false,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd = nil,
+    args = {}
+  },
+  view = {
+    width = 30,
+    height = 30,
+    side = "left",
+    auto_resize = true,
+    mappings = {
+      custom_only = false,
+      list = {}
     }
+  }
 }
 
 local setmap = vim.api.nvim_set_keymap
 local opt = {noremap = true}
 
-setmap('n', '<C-n>', ':NvimTreeToggle<CR>', opt)
--- NvimTreeOpen and NvimTreeClose are also available if you need them
-
--- a list of groups can be found at `:help nvim_tree_highlight`
--- vim.cmd [[highlight NvimTreeFolderIcon guibg=blue]]
+setmap("n", "<C-n>", ":NvimTreeToggle<CR>", opt)
