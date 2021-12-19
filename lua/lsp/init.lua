3c971059e22a4b96fd8a46aed75a3cf1137b0bc9
Ben Hunt <bigben.hunt@gmail.com>
2021-12-19 01:23:13 -0700

    formatting

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,17 +1,17 @@
-require "lsp.keymap"
-require "lsp.vsnip-conf"
-require "lsp.cmp-conf"
-require "lsp.lspkind-conf"
-require "lsp.lspsaga-conf"
+require("lsp.keymap")
+require("lsp.vsnip-conf")
+require("lsp.cmp-conf")
+require("lsp.lspkind-conf")
+require("lsp.lspsaga-conf")
 
-local signature_config = require "lsp.signature"
+local signature_config = require("lsp.signature")
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local function documentHighlight(client, bufnr)
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
+	if client.resolved_capabilities.document_highlight then
+		vim.api.nvim_exec(
+			[[
       hi LspDiagnosticsVirtualTextInformation term=bold guifg='#51afef' guibg='#202328'
       hi LspDiagnosticsVirtualTextWarning term=bold guifg='#ecbe7b' guibg='#202328'
       hi LspDiagnosticsVirtualTextHint term=bold guifg='#98c379' guibg='#202328'
@@ -22,61 +22,59 @@ local function documentHighlight(client, bufnr)
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
       ]],
-      false
-    )
-  end
+			false
+		)
+	end
 end
 
 local function on_attach(client, bufnr)
-  documentHighlight(client, bufnr)
-  require "lsp_signature".on_attach(signature_config)
+	documentHighlight(client, bufnr)
+	require("lsp_signature").on_attach(signature_config)
 end
 
 local lua_settings = {
-  Lua = {
-    runtime = {
-      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-      version = "LuaJIT",
-      -- Setup your lua path
-      path = vim.split(package.path, ";")
-    },
-    diagnostics = {
-      -- Get the language server to recognize the `vim` global
-      globals = {"vim"}
-    },
-    workspace = {
-      -- Make the server aware of Neovim runtime files
-      library = {
-        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
-      }
-    }
-  }
+	Lua = {
+		runtime = {
+			-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+			version = "LuaJIT",
+			-- Setup your lua path
+			path = vim.split(package.path, ";"),
+		},
+		diagnostics = {
+			-- Get the language server to recognize the `vim` global
+			globals = { "vim" },
+		},
+		workspace = {
+			-- Make the server aware of Neovim runtime files
+			library = {
+				[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+				[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+			},
+		},
+	},
 }
 
-local rust_settings ={ }
+local rust_settings = {}
 
 local function my_setup()
-  return {
-    capabilities = capabilities,
-    on_attach = on_attach
-  }
+	return {
+		capabilities = capabilities,
+		on_attach = on_attach,
+	}
 end
 
 local function setup_servers()
-  local lspinstaller = require "nvim-lsp-installer"
-  lspinstaller.on_server_ready(
-    function(server)
-      local config = my_setup()
-      if server.name == "sumneko_lua" then
-        config.settings = lua_settings
-      end
-      if server.name == "rust_analyzer" then
-        config.settings = rust_settings
-      end
-      server:setup(config)
-    end
-  )
+	local lspinstaller = require("nvim-lsp-installer")
+	lspinstaller.on_server_ready(function(server)
+		local config = my_setup()
+		if server.name == "sumneko_lua" then
+			config.settings = lua_settings
+		end
+		if server.name == "rust_analyzer" then
+			config.settings = rust_settings
+		end
+		server:setup(config)
+	end)
 end
 
 setup_servers()
--- plugin/commands.vim
+++ plugin/commands.vim
@@ -1,1 +1,2 @@
 command BaseLSInstall lua require("lsp.base-install-ls").auto_install_lsp()
+command Format lua vim.lsp.buf.formatting()
