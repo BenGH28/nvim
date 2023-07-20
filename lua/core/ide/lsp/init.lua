a92061a0640d2b1f79f7f72e54220af09a9d61c9
Ben Hunt <bnjmnhnt.git@gmail.com>
2023-07-19 20:41:25 -0600

    formatting

--- lua/core/ide/lsp/init.lua
+++ lua/core/ide/lsp/init.lua
@@ -6,151 +6,151 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protoc
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 vim.diagnostic.config {
-  virtual_text = {
-    prefix = "●",
-  },
+	virtual_text = {
+		prefix = "●",
+	},
 }
 
 local augroup = vim.api.nvim_create_augroup
 local au = vim.api.nvim_create_autocmd
 
 local function documentHighlight(client, bufnr)
-  if client.server_capabilities.documentHighlightProvider then
-    local hl = vim.api.nvim_set_hl
-    hl(0, "LspDiagnosticsVirtualTextInformation", { bold = true, fg = "#51afef", bg = "#202328" })
-    hl(0, "LspDiagnosticsVirtualTextWarning", { bold = true, fg = "#ecbe7b", bg = "#202328" })
-    hl(0, "LspDiagnosticsVirtualTextHint", { bold = true, fg = "#98c379", bg = "#202328" })
-    hl(0, "LspDiagnosticsVirtualTextError", { bold = true, fg = "#ec5f67", bg = "#202328" })
+	if client.server_capabilities.documentHighlightProvider then
+		local hl = vim.api.nvim_set_hl
+		hl(0, "LspDiagnosticsVirtualTextInformation", { bold = true, fg = "#51afef", bg = "#202328" })
+		hl(0, "LspDiagnosticsVirtualTextWarning", { bold = true, fg = "#ecbe7b", bg = "#202328" })
+		hl(0, "LspDiagnosticsVirtualTextHint", { bold = true, fg = "#98c379", bg = "#202328" })
+		hl(0, "LspDiagnosticsVirtualTextError", { bold = true, fg = "#ec5f67", bg = "#202328" })
 
-    local doc_highlight = augroup("lsp_document_highlight", {
-      clear = false,
-    })
-    vim.api.nvim_clear_autocmds {
-      pattern = "*",
-      group = doc_highlight,
-    }
+		local doc_highlight = augroup("lsp_document_highlight", {
+			clear = false,
+		})
+		vim.api.nvim_clear_autocmds {
+			pattern = "*",
+			group = doc_highlight,
+		}
 
-    au({ "CursorHold", "CursorHoldI" }, {
-      group = doc_highlight,
-      pattern = "*",
-      callback = vim.lsp.buf.document_highlight,
-    })
-    au({ "CursorMoved", "CursorMovedI" }, {
-      group = doc_highlight,
-      pattern = "*",
-      callback = vim.lsp.buf.clear_references,
-    })
-  end
+		au({ "CursorHold", "CursorHoldI" }, {
+			group = doc_highlight,
+			pattern = "*",
+			callback = vim.lsp.buf.document_highlight,
+		})
+		au({ "CursorMoved", "CursorMovedI" }, {
+			group = doc_highlight,
+			pattern = "*",
+			callback = vim.lsp.buf.clear_references,
+		})
+	end
 end
 
 local function format_on_save()
-  local format = augroup("format", { clear = true })
-  vim.api.nvim_clear_autocmds {
-    pattern = "*",
-    group = format,
-  }
+	local format = augroup("format", { clear = true })
+	vim.api.nvim_clear_autocmds {
+		pattern = "*",
+		group = format,
+	}
 
-  -- aha now we have format on save
-  au("BufWritePre", {
-    pattern = "*",
-    callback = function()
-      vim.lsp.buf.format { async = false }
-    end,
-    group = format,
-  })
+	-- aha now we have format on save
+	au("BufWritePre", {
+		pattern = "*",
+		callback = function()
+			vim.lsp.buf.format { async = false }
+		end,
+		group = format,
+	})
 end
 
 local function on_attach(client, bufnr)
-  documentHighlight(client, bufnr)
-  if client.server_capabilities.documentSymbolsProvider then
-    require("nvim-navic").attach(client, bufnr)
-  end
-  format_on_save()
+	documentHighlight(client, bufnr)
+	if client.server_capabilities.documentSymbolsProvider then
+		require("nvim-navic").attach(client, bufnr)
+	end
+	format_on_save()
 end
 
 local lua_settings = {
-  Lua = {
-    runtime = {
-      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-      version = "LuaJIT",
-      -- Setup your lua path
-      path = vim.split(package.path, ";"),
-    },
-    diagnostics = {
-      -- Get the language server to recognize the `vim` global
-      globals = { "vim", "awesome" },
-    },
-    workspace = {
-      -- Make the server aware of Neovim runtime files
-      library = {
-        [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-        [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-      },
-    },
-  },
+	Lua = {
+		runtime = {
+			-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+			version = "LuaJIT",
+			-- Setup your lua path
+			path = vim.split(package.path, ";"),
+		},
+		diagnostics = {
+			-- Get the language server to recognize the `vim` global
+			globals = { "vim", "awesome" },
+		},
+		workspace = {
+			-- Make the server aware of Neovim runtime files
+			library = {
+				[vim.fn.expand "$VIMRUNTIME/lua"] = true,
+				[vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+			},
+		},
+	},
 }
 
 local function rust_opts()
-  return {
-    server = {
-      on_attach = function(_, bufnr)
-        vim.keymap.set(
-          "n",
-          "<leader-dk>",
-          require("rust-tools").hover_actions.hover_actions,
-          { buffer = bufnr }
-        )
-      end,
-    },
-    tools = {
-      -- rust-tools options
-      autoSetHints = true,
-      inlay_hints = {
-        only_current_line = false,
-        show_parameter_hints = true,
-      },
-    },
-  }
+	return {
+		server = {
+			on_attach = function(_, bufnr)
+				vim.keymap.set(
+					"n",
+					"<leader-dk>",
+					require("rust-tools").hover_actions.hover_actions,
+					{ buffer = bufnr }
+				)
+			end,
+		},
+		tools = {
+			-- rust-tools options
+			autoSetHints = true,
+			inlay_hints = {
+				only_current_line = false,
+				show_parameter_hints = true,
+			},
+		},
+	}
 end
 
 local function server_config()
-  return {
-    capabilities = capabilities,
-    on_attach = on_attach,
-  }
+	return {
+		capabilities = capabilities,
+		on_attach = on_attach,
+	}
 end
 
 local function setup_servers()
-  local servers = {
-    "rust_analyzer",
-    "bashls",
-    "clangd",
-    "html",
-    "ltex",
-    "jsonls",
-    "pyright",
-    "lua_ls",
-    "tsserver",
-    "gopls",
-  }
+	local servers = {
+		"rust_analyzer",
+		"bashls",
+		"clangd",
+		"html",
+		"ltex",
+		"jsonls",
+		"pyright",
+		"lua_ls",
+		"tsserver",
+		"gopls",
+	}
 
-  require("neodev").setup()
-  require("mason").setup()
-  require("mason-lspconfig").setup { ensure_installed = servers, automatic_installation = true }
-  local installed = require("mason-lspconfig").get_installed_servers() or servers
+	require("neodev").setup()
+	require("mason").setup()
+	require("mason-lspconfig").setup { ensure_installed = servers, automatic_installation = true }
+	local installed = require("mason-lspconfig").get_installed_servers() or servers
 
-  for _, server in pairs(installed) do
-    local config = server_config()
-    if server == "lua_ls" then
-      config.settings = lua_settings
-    end
+	for _, server in pairs(installed) do
+		local config = server_config()
+		if server == "lua_ls" then
+			config.settings = lua_settings
+		end
 
-    if server == "rust_analyzer" then
-      require("rust-tools").setup(rust_opts())
-    else
-      require("lspconfig")[server].setup(config)
-    end
-  end
+		if server == "rust_analyzer" then
+			require("rust-tools").setup(rust_opts())
+		else
+			require("lspconfig")[server].setup(config)
+		end
+	end
 end
 
 setup_servers()
--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -1,97 +1,96 @@
 return {
 
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require "core.ide.lsp"
-    end,
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require "core.ide.lsp"
+		end,
 
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        build = ":MasonUpdate",
-      },
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				build = ":MasonUpdate",
+			},
 
-      {
-        "williamboman/mason-lspconfig.nvim",
-      },
+			{
+				"williamboman/mason-lspconfig.nvim",
+			},
 
-      -- snippet fill
-      { "rafamadriz/friendly-snippets" },
+			-- snippet fill
+			{ "rafamadriz/friendly-snippets" },
 
-      -- pictograms for the lsp
-      { "onsails/lspkind-nvim" },
+			-- pictograms for the lsp
+			{ "onsails/lspkind-nvim" },
 
-      -- lsp front end that looks nice
-      { "tami5/lspsaga.nvim" },
+			-- lsp front end that looks nice
+			{ "tami5/lspsaga.nvim" },
 
+			-- rust ide stuff
+			{ "simrat39/rust-tools.nvim" },
 
-      -- rust ide stuff
-      { "simrat39/rust-tools.nvim" },
+			-- diagnostics
+			{ "folke/trouble.nvim" },
 
-      -- diagnostics
-      { "folke/trouble.nvim" },
+			-- neovim help
+			{ "folke/neodev.nvim" },
 
-      -- neovim help
-      { "folke/neodev.nvim" },
+			-- get some snippet support
+			{
+				"hrsh7th/vim-vsnip",
+				setup = function()
+					vim.g.vsnip_snippet_dir = vim.fn.stdpath "config" .. "/vsnip"
+				end,
+			},
+			{
+				"mfussenegger/nvim-dap",
+			},
+		},
+	},
 
-      -- get some snippet support
-      {
-        "hrsh7th/vim-vsnip",
-        setup = function()
-          vim.g.vsnip_snippet_dir = vim.fn.stdpath "config" .. "/vsnip"
-        end,
-      },
-      {
-        "mfussenegger/nvim-dap",
-      },
-    },
-  },
+	{
+		"vim-scripts/DoxygenToolkit.vim",
+		ft = { "cpp", "c", "javascript" },
+		config = function()
+			require "core.ide.doxygen"
+		end,
+	},
 
-  {
-    "vim-scripts/DoxygenToolkit.vim",
-    ft = { "cpp", "c", "javascript" },
-    config = function()
-      require "core.ide.doxygen"
-    end,
-  },
+	{
+		"Exafunction/codeium.vim",
 
-  {
-    "Exafunction/codeium.vim",
+		event = "InsertEnter",
+		config = function()
+			-- Change '<C-g>' here to any keycode you like.
+			vim.keymap.set("i", "<C-g>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true })
+			vim.keymap.set("i", "<c-;>", function()
+				return vim.fn["codeium#CycleCompletions"](1)
+			end, { expr = true })
+			vim.keymap.set("i", "<c-,>", function()
+				return vim.fn["codeium#CycleCompletions"](-1)
+			end, { expr = true })
+			vim.keymap.set("i", "<c-x>", function()
+				return vim.fn["codeium#Clear"]()
+			end, { expr = true })
+		end,
+	},
 
-    event = "InsertEnter",
-    config = function()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set("i", "<C-g>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true })
-      vim.keymap.set("i", "<c-;>", function()
-        return vim.fn["codeium#CycleCompletions"](1)
-      end, { expr = true })
-      vim.keymap.set("i", "<c-,>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, { expr = true })
-      vim.keymap.set("i", "<c-x>", function()
-        return vim.fn["codeium#Clear"]()
-      end, { expr = true })
-    end,
-  },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require "core.ide.autopairs"
+		end,
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require "core.ide.autopairs"
-    end,
-  },
-
-  -- Session persistence
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre", -- this will only start session saving when an actual file was opened
-    config = function()
-      require("persistence").setup()
-    end,
-  },
+	-- Session persistence
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		config = function()
+			require("persistence").setup()
+		end,
+	},
 }
