cf19e73ac44c22d7fd21ca1373b299a5bb7d5b38
Ben Hunt <bnjmnhnt.git@gmail.com>
2023-07-20 20:28:11 -0600

    improve style

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+my_nvim="$HOME/.config/nvim"
+backup="$HOME/.config/nvim.backup"
+
 clone_my_nvim_repo() {
-    if [ -d $HOME/.config/nvim ]; then
-        echo "backing up your old config"
-        mv $HOME/.config/nvim $HOME/.config/nvim.bak
-    fi
-    echo "cloning repo to '$HOME/.config/nvim'"
-    git clone https://github.com/BenGH28/nvim.git $HOME/.config/nvim/
+	if [ -d "$HOME/.config/nvim" ]; then
+		echo "backing up your old config"
+		mv "$my_nvim" "$backup"
+	fi
+	echo "cloning repo to '$my_nvim'"
+	git clone https://github.com/BenGH28/nvim.git "$my_nvim"
 }
 
 clone_my_nvim_repo
