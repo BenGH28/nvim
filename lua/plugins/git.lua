0d4caad4eca6d1759d75aa457a7bd07f5f4ee78d
Ben Hunt <bnjmnhnt.git@gmail.com>
2023-09-07 18:43:30 -0600

    setup gitsigns to show the gutter for git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,17 +1,24 @@
 return {
 
-	{ "lewis6991/gitsigns.nvim", event = "BufEnter" },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufEnter",
+    config = function()
+      require("gitsigns").setup {}
+    end
+  },
 
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = "LazyGit",
-	},
 
-	{
-		"APZelos/blamer.nvim",
-		event = "BufEnter",
-		init = function()
-			vim.g.blamer_enabled = 1
-		end,
-	},
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+  },
+
+  {
+    "APZelos/blamer.nvim",
+    event = "BufEnter",
+    init = function()
+      vim.g.blamer_enabled = 1
+    end,
+  },
 }
