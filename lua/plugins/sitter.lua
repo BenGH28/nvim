fa89d2173ff2d59880bcf49e3341f4cba969eebc
Ben Hunt <bnjmnhnt.git@gmail.com>
2023-09-15 14:53:43 -0600

    add treesitter context plugin

--- lua/plugins/sitter.lua
+++ lua/plugins/sitter.lua
@@ -8,6 +8,7 @@ return {
 			"windwp/nvim-ts-autotag",
 			"p00f/nvim-ts-rainbow",
 			"nvim-treesitter/nvim-treesitter-textobjects",
+			"nvim-treesitter/nvim-treesitter-context",
 		},
 		config = function()
 			require "core.treesitter"
