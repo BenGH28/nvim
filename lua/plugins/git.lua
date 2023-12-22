c920ea6a3a4161b3a0c240c06d755367f63cd9ab
Ben Hunt <bnjmnhnt.git@gmail.com>
2023-12-22 11:22:00 -0700

    remove blamer.nvim
    gitsigns provides the same functionality with better implementation

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,26 +8,21 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "BufEnter",
     config = function()
-      require("gitsigns").setup {}
+      require("gitsigns").setup {
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
+          virt_text_priority = 100,
+        },
+      }
     end,
   },
 
   {
     "kdheepak/lazygit.nvim",
     cmd = "LazyGit",
-  },
-
-  {
-    "APZelos/blamer.nvim",
-    cond = vim.loop.os_uname().sysname == "Linux",
-    config = function()
-      require("blamer").setup()
-    end,
-
-    event = "BufEnter",
-    commit = "afd2fde1989b84f4a69639f25d8f4f60519b5cc7",
-    init = function()
-      vim.g.blamer_enabled = true
-    end,
   },
 }
