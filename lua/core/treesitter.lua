a59fd7b87b09c8a936250139d1fa9186d00f1e27
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-06-03 22:56:46 -0600

    move the TSRefreshLang to the treesitter config file

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -113,3 +113,26 @@ local opts = {
 }
 
 require("nvim-treesitter.configs").setup(opts)
+
+-- a command to help fix this issue: https://github.com/nvim-treesitter/nvim-treesitter/issues/3092
+-- clear a language parser and its associated queries
+vim.api.nvim_create_user_command("TSRefreshLang", function(args)
+  local lang = args.args
+  local libs = vim.api.nvim_get_runtime_file("parser/" .. lang .. ".so", true)
+  local queries = vim.api.nvim_get_runtime_file("queries/" .. lang .. ".scm", true)
+
+  for _, lib in ipairs(libs) do
+    local cmd = "rm -f " .. lib
+    -- ignore files in /usr/share/nvim/parser
+    if vim.fn.fnamemodify(lib, ":h") ~= "/usr/share/nvim/parser" then
+      os.execute(cmd)
+    end
+  end
+
+  for _, query in ipairs(queries) do
+    local cmd = "rm -f " .. query
+    os.execute(cmd)
+  end
+
+  vim.cmd("TSInstall! " .. lang)
+end, { force = true, nargs = 1 })
