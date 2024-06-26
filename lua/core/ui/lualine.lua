a11f57de1dc576625e1f0e2faed951b454d165a5
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-06-25 22:36:01 -0600

    get rid of the evilline/glepnir lualine rip off

--- lua/core/ui/lualine.lua
+++ lua/core/ui/lualine.lua
@@ -1,151 +1,6 @@
--- Author: shadmansaleh
--- Credit: glepnir
 local lualine = require "lualine"
 
--- Color table for highlights
--- stylua: ignore
-local colors = {
-  bg       = '#202328',
-  fg       = '#bbc2cf',
-  yellow   = '#E5C07B',
-  cyan     = '#008080',
-  darkblue = '#081633',
-  green    = '#98C379',
-  orange   = '#FF8800',
-  violet   = '#a9a1e1',
-  magenta  = '#c678dd',
-  blue     = '#61afef',
-  red      = '#E06C75',
-}
-
-local conditions = {
-  buffer_not_empty = function()
-    return vim.fn.empty(vim.fn.expand "%:t") ~= 1
-  end,
-  hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
-  end,
-  check_git_workspace = function()
-    local filepath = vim.fn.expand "%:p:h"
-    local gitdir = vim.fn.finddir(".git", filepath .. ";")
-    return gitdir and #gitdir > 0 and #gitdir < #filepath
-  end,
-}
-
--- Config
-local config = {
-  options = {
-    -- Disable sections and component separators
-    component_separators = "",
-    section_separators = "",
-    icons_enabled = true,
-    theme = {
-      -- We are going to use lualine_c an lualine_x as left and
-      -- right section. Both are highlighted by c theme .  So we
-      -- are just setting default looks o statusline
-      normal = { c = { fg = colors.fg, bg = colors.bg } },
-      inactive = { c = { fg = colors.fg, bg = colors.bg } },
-    },
-    disabled_filetypes = { "NvimTree", "startify", "packer" },
-  },
-  sections = {
-    -- these are to remove the defaults
-    lualine_a = {},
-    lualine_b = {},
-    lualine_y = {},
-    lualine_z = {},
-    -- These will be filled later
-    lualine_c = {},
-    lualine_x = {},
-  },
-  inactive_sections = {
-    -- these are to remove the defaults
-    lualine_a = {},
-    lualine_b = {},
-    lualine_y = {},
-    lualine_z = {},
-    lualine_c = {},
-    lualine_x = {},
-  },
-}
-
--- Inserts a component in lualine_c at left section
-local function ins_left(component)
-  table.insert(config.sections.lualine_c, component)
-end
-
--- Inserts a component in lualine_x ot right section
-local function ins_right(component)
-  table.insert(config.sections.lualine_x, component)
-end
-
-ins_left {
-  -- mode component
-  function()
-    -- auto change color according to neovims mode
-    local mode_color = {
-      n = colors.red,
-      i = colors.green,
-      v = colors.blue,
-      [""] = colors.blue,
-      V = colors.blue,
-      c = colors.magenta,
-      no = colors.red,
-      s = colors.orange,
-      S = colors.orange,
-      [""] = colors.orange,
-      ic = colors.yellow,
-      R = colors.violet,
-      Rv = colors.violet,
-      cv = colors.red,
-      ce = colors.red,
-      r = colors.cyan,
-      rm = colors.cyan,
-      ["r?"] = colors.cyan,
-      ["!"] = colors.red,
-      t = colors.red,
-    }
-    vim.api.nvim_command("hi! LualineMode guifg=" .. mode_color[vim.fn.mode()] .. " guibg=" .. colors.bg)
-    return string.upper(vim.api.nvim_get_mode().mode)
-  end,
-  color = "LualineMode",
-  padding = { right = 1 },
-}
-
-ins_left {
-  -- filesize component
-  "filesize",
-  cond = conditions.buffer_not_empty,
-}
-
-ins_left {
-  "filetype",
-  colored = true,
-  icon_only = true,
-}
-
-ins_left {
-  function()
-    return vim.fn.pathshorten(vim.fn.expand('%:p'))
-  end,
-  cond = conditions.buffer_not_empty,
-  color = { fg = colors.magenta, gui = "bold" },
-}
-
-ins_left {
-  "diagnostics",
-  sources = { "nvim_diagnostic" },
-  symbols = { error = " ", warn = " ", info = " " },
-  colored = true,
-  diagnostics_color = {
-    color_error = { fg = colors.red },
-    color_warn = { fg = colors.yellow },
-    color_info = { fg = colors.cyan },
-  },
-}
-
-ins_right {
-  -- Lsp server name .
+local lsp_component = {
   function()
     local msg = "No Active Lsp"
     local clients = vim.lsp.get_clients({ bufnr = 0 })
@@ -162,39 +17,54 @@ ins_right {
     end
     return msg
   end,
-  icon = "󰒋",
-  color = { fg = colors.red, gui = "bold" },
+  icon = ' ',
 }
 
-ins_right { "progress", color = { fg = colors.fg, gui = "bold" } }
 
-ins_right {
-  "fileformat",
-  symbols = {
-    unix = "", -- e712
-    dos = "", -- e70f
-    mac = "", -- e711
+lualine.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
   },
-  icons_enabled = true,
-  color = { fg = colors.green, gui = "bold" },
-}
-
-ins_right {
-  "branch",
-  icon = "󰘬",
-  color = { fg = colors.violet, gui = "bold" },
-}
-
-ins_right {
-  "diff",
-  symbols = { added = " ", modified = " ", removed = " " },
-  diff_color = {
-    added = { fg = colors.green },
-    modified = { fg = colors.orange },
-    removed = { fg = colors.red },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { { 'branch', icon = '󰊢' }, 'diff', 'diagnostics' },
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = { 'encoding', 'fileformat', 'filetype', },
+    lualine_y = { 'progress', },
+    lualine_z = { 'location' }
   },
-  cond = conditions.hide_in_width,
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {
+    lualine_a = { 'buffers' },
+    lualine_b = { lsp_component },
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'tabs' }
+  },
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
-
--- Now don't forget to initialize lualine
-lualine.setup(config)
