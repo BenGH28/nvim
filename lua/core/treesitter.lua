8396a8184e665f791f13c11b3defa477bddf9532
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-06-26 14:38:04 -0600

    remove norg stuff

--- lua/core/treesitter.lua
+++ lua/core/treesitter.lua
@@ -1,23 +1,5 @@
 local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
 
--- These two are optional and provide syntax highlighting
--- for Neorg tables and the @document.meta tag
-parser_configs.norg_meta = {
-  install_info = {
-    url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-    files = { "src/parser.c" },
-    branch = "main",
-  },
-}
-
-parser_configs.norg_table = {
-  install_info = {
-    url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-    files = { "src/parser.c" },
-    branch = "main",
-  },
-}
-
 local languages = {
   "c",
   "cpp",
@@ -27,10 +9,9 @@ local languages = {
   "toml",
   "typescript",
   "tsx",
-  -- "vim",
+  "nu",
   "yaml",
   "python",
-  "norg",
   "javascript",
   "css",
   "html",
