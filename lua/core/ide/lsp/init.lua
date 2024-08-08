3aec22683b5a1dc128c48ffcabccf53b154af20c
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-08-08 15:53:05 -0600

    ruff doesn't catch everything, let pyright have a go

--- lua/core/ide/lsp/init.lua
+++ lua/core/ide/lsp/init.lua
@@ -176,12 +176,6 @@ local function setup_servers()
           -- Using Ruff's import organizer
           disableOrganizeImports = true,
         },
-        python = {
-          analysis = {
-            -- Ignore all files for analysis to exclusively use Ruff for linting
-            ignore = { '*' },
-          },
-        },
       }
     end
 
