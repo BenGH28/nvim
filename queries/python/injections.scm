dfa601b5a50f3cce86258091d96a5e1479684dd0
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-08-16 15:54:37 -0600

    queries: better injections for embedded sql in python

--- queries/python/injections.scm
+++ queries/python/injections.scm
@@ -1,36 +1,161 @@
+(return_statement
+  (string
+    (string_content) @injection.content (#any-contains? @injection.content
+                                         "ALTER"
+                                         "AND"
+                                         "CREATE"
+                                         "DELETE"
+                                         "DROP"
+                                         "FROM"
+                                         "INSERT"
+                                         "JOIN"
+                                         "NOT"
+                                         "NULL"
+                                         "OR"
+                                         "SELECT"
+                                         "TRUNCATE"
+                                         "UPDATE"
+                                         "WHERE"
+                                         "alter"
+                                         "and"
+                                         "create"
+                                         "delete"
+                                         "drop"
+                                         "from"
+                                         "insert"
+                                         "join"
+                                         "not"
+                                         "null"
+                                         "or"
+                                         "select"
+                                         "truncate"
+                                         "update"
+                                         "where")
+    )
+  (#set! injection.language "sql")
+  (#set! injection.include-children)
+  )
+
+(return_statement
+  (call
+    function: (
+               attribute
+               object: (string
+                         (string_content) @injection.content (#any-contains? @injection.content
+                                                              "ALTER"
+                                                              "AND"
+                                                              "CREATE"
+                                                              "DELETE"
+                                                              "DROP"
+                                                              "FROM"
+                                                              "INSERT"
+                                                              "JOIN"
+                                                              "NOT"
+                                                              "NULL"
+                                                              "OR"
+                                                              "SELECT"
+                                                              "TRUNCATE"
+                                                              "UPDATE"
+                                                              "WHERE"
+                                                              "alter"
+                                                              "and"
+                                                              "create"
+                                                              "delete"
+                                                              "drop"
+                                                              "from"
+                                                              "insert"
+                                                              "join"
+                                                              "not"
+                                                              "null"
+                                                              "or"
+                                                              "select"
+                                                              "truncate"
+                                                              "update"
+                                                              "where")
+                         )
+               )
+    )
+  (#set! injection.language "sql")
+  (#set! injection.include-children)
+  )
+
+(assignment
+  right: (call
+           function: (
+                      attribute
+                      object: (string
+                                (string_content) @injection.content (#any-contains? @injection.content
+                                                                     "ALTER"
+                                                                     "AND"
+                                                                     "CREATE"
+                                                                     "DELETE"
+                                                                     "DROP"
+                                                                     "FROM"
+                                                                     "INSERT"
+                                                                     "JOIN"
+                                                                     "NOT"
+                                                                     "NULL"
+                                                                     "OR"
+                                                                     "SELECT"
+                                                                     "TRUNCATE"
+                                                                     "UPDATE"
+                                                                     "WHERE"
+                                                                     "alter"
+                                                                     "and"
+                                                                     "create"
+                                                                     "delete"
+                                                                     "drop"
+                                                                     "from"
+                                                                     "insert"
+                                                                     "join"
+                                                                     "not"
+                                                                     "null"
+                                                                     "or"
+                                                                     "select"
+                                                                     "truncate"
+                                                                     "update"
+                                                                     "where")
+                                )
+                      )
+           )
+  (#set! injection.language "sql")
+  (#set! injection.include-children)
+  )
 
   (assignment
     right: (string
              (string_content) @injection.content (#any-contains? @injection.content
-                                        "select"
-                                        "from"
-                                        "insert"
-                                        "update"
-                                        "delete"
-                                        "create"
-                                        "drop"
-                                        "alter"
-                                        "join"
-                                        "where"
-                                        "and"
-                                        "or"
-                                        "not"
-                                        "null"
-                                        "SELECT"
-                                        "FROM"
-                                        "INSERT"
-                                        "UPDATE"
-                                        "DELETE"
-                                        "CREATE"
-                                        "DROP"
-                                        "ALTER"
-                                        "JOIN"
-                                        "WHERE"
-                                        "AND"
-                                        "OR"
-                                        "NOT"
-                                        "NULL")
+                                                  "ALTER"
+                                                  "AND"
+                                                  "CREATE"
+                                                  "DELETE"
+                                                  "DROP"
+                                                  "FROM"
+                                                  "INSERT"
+                                                  "JOIN"
+                                                  "NOT"
+                                                  "NULL"
+                                                  "OR"
+                                                  "SELECT"
+                                                  "TRUNCATE"
+                                                  "UPDATE"
+                                                  "WHERE"
+                                                  "alter"
+                                                  "and"
+                                                  "create"
+                                                  "delete"
+                                                  "drop"
+                                                  "from"
+                                                  "insert"
+                                                  "join"
+                                                  "not"
+                                                  "null"
+                                                  "or"
+                                                  "select"
+                                                  "truncate"
+                                                  "update"
+                                                  "where")
              )
-      (#set! injection.language "sql")
-      (#set! injection.include-children)
+    (#set! injection.language "sql")
+    (#set! injection.include-children)
     )
