4a5e67a8879a66273864b9e94ceea0fc2718674f
Ben Hunt <bnjmnhnt.git@gmail.com>
2024-09-23 12:59:00 -0600

    manually enable codeium

--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -73,7 +73,7 @@ return {
 
   {
     "Exafunction/codeium.vim",
-    event = "InsertEnter",
+    cmd = "Codeium",
     config = function()
       -- Change '<C-g>' here to any keycode you like.
       vim.keymap.set("i", "<C-g>", function()
