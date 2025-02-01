d9af1269fbd23e932d99a294eb7e935a914768bd
Ben Hunt <bnjmnhnt.git@gmail.com>
2025-01-31 23:05:23 -0700

    add ftplugin for markdown
    add keybinding and user command for previewing markdown file with `glow`

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+local glow = function()
+  local filename = vim.api.nvim_buf_get_name(0)
+  local koin = require "koin"
+  local cmd = "glow -p " .. filename
+  koin.show(cmd)
+end
+
+vim.keymap.set("n", "<leader>fm", glow)
+
+vim.api.nvim_buf_create_user_command(0, "Glow", glow, {})
