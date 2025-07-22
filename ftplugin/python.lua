9e110cc8d890933288ca1dcf9304799109a8a103
Ben Hunt <bnjmnhnt.git@gmail.com>
2025-07-22 13:16:37 -0600

    ftplugin(python): add some maps for inserting type statements

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,4 +1,5 @@
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 
-vim.keymap.set("n", "<leader>i", "A #type: ignore<esc>", { desc = "insert ignore statement", buffer = true })
+vim.keymap.set("n", "<leader>pti", "A # type: ignore<esc>", { desc = "insert ignore statement", buffer = true })
+vim.keymap.set("n", "<leader>ptt", "A # type: ", { desc = "insert open-ended type statement", buffer = true })
