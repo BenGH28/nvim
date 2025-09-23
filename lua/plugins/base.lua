6396074608a2d9048ba2c32ef44d02ba0363d8c9
Ben Hunt <bnjmnhnt.git@gmail.com>
2025-09-23 11:42:16 -0600

    remove impatient and filetype.nvim
    all part of neovime core

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,16 +1,4 @@
 return {
-  -- better loading of files
-  {
-    "lewis6991/impatient.nvim",
-    priority = 2000,
-    lazy = false,
-    config = function()
-      require "impatient"
-    end,
-  },
-  -- faster filetype
-  { "nathom/filetype.nvim", lazy = false },
-
   ---[[General Dependencies
   { "nvim-lua/popup.nvim" },
   { "nvim-lua/plenary.nvim" },
