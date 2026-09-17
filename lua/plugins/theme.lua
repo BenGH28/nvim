b9573c01975d519352e7d246c2972e3effd8542a
Ben Hunt <bnjmnhnt.git@gmail.com>
2026-09-16 20:15:44 -0600

    making it mocha

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,7 +6,7 @@ return {
     priority = 1000,
     config = function()
       require("catppuccin").setup()
-      vim.cmd.colorscheme 'catppuccin-latte'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   }
 }
